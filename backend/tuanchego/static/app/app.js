0061746aab275f8e19083bb3a764c15a58370a9a
tommy <lion_awake@163.com>
2017-03-05 17:54:58 +0800

    css-

--- backend/tuanchego/static/app/app.js
+++ backend/tuanchego/static/app/app.js
@@ -16879,27 +16879,36 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
 (function(angular){'use strict';"use strict";
 
 (function e(t, n, r) {
-    function s(o, u) {
-        if (!n[o]) {
-            if (!t[o]) {
-                var a = typeof require == "function" && require;if (!u && a) return a(o, !0);if (i) return i(o, !0);var f = new Error("Cannot find module '" + o + "'");throw f.code = "MODULE_NOT_FOUND", f;
-            }var l = n[o] = { exports: {} };t[o][0].call(l.exports, function (e) {
-                var n = t[o][1][e];return s(n ? n : e);
-            }, l, l.exports, e, t, n, r);
-        }return n[o].exports;
-    }var i = typeof require == "function" && require;for (var o = 0; o < r.length; o++) {
-        s(r[o]);
-    }return s;
+				function s(o, u) {
+								if (!n[o]) {
+												if (!t[o]) {
+																var a = typeof require == "function" && require;if (!u && a) return a(o, !0);if (i) return i(o, !0);var f = new Error("Cannot find module '" + o + "'");throw f.code = "MODULE_NOT_FOUND", f;
+												}var l = n[o] = { exports: {} };t[o][0].call(l.exports, function (e) {
+																var n = t[o][1][e];return s(n ? n : e);
+												}, l, l.exports, e, t, n, r);
+								}return n[o].exports;
+				}var i = typeof require == "function" && require;for (var o = 0; o < r.length; o++) {
+								s(r[o]);
+				}return s;
 })({ 1: [function (require, module, exports) {
-        angular.module('app.base').controller('headerController', headerController);
+								angular.module('app.base').controller('headerController', headerController);
 
-        headerController.$inject = ['$scope'];
-        function headerController($scope) {
-            $scope.$on('$includeContentLoaded', function () {
-                Layout.initHeader(); // init header
-            });
-        }
-    }, {}] }, {}, [1]);})(angular);
+								headerController.$inject = ['$scope', '$rootScope'];
+								function headerController($scope, $rootScope) {
+												$scope.$on('$includeContentLoaded', function () {
+																Layout.initHeader(); // init header
+												});
+												/*$rootScope.$on('$stateChangeStart', 
+            function(event, toState, toParams, fromState, fromParams){ 
+             // do something
+             if(toState!='base.home'){
+             	$scrop.navRegSlidVisible=false;
+             }else{
+             	$scrop.navRegSlidVisible=true;
+             }
+            });*/
+								}
+				}, {}] }, {}, [1]);})(angular);
 (function(angular){'use strict';"use strict";
 
 (function e(t, n, r) {
--- frontend/app/base/controllers/header.js
+++ frontend/app/base/controllers/header.js
@@ -1,8 +1,17 @@
 angular.module('app.base').controller('headerController', headerController);
 
-headerController.$inject = ['$scope'];
-function headerController($scope) {
+headerController.$inject = ['$scope','$rootScope'];
+function headerController($scope, $rootScope) {
     $scope.$on('$includeContentLoaded', function() {
         Layout.initHeader(); // init header
     });
+    /*$rootScope.$on('$stateChangeStart', 
+	function(event, toState, toParams, fromState, fromParams){ 
+	    // do something
+	    if(toState!='base.home'){
+	    	$scrop.navRegSlidVisible=false;
+	    }else{
+	    	$scrop.navRegSlidVisible=true;
+	    }
+	});*/
 }
